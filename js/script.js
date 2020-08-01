9d10ea0971ee8007ce89af533bed772353799aed
ivanzapatarivera <ivan.zapata.rivera@gmail.com>
2020-08-01 17:20:09 -0400

    organized comments and JS structure

--- js/script.js
+++ js/script.js
@@ -1,104 +1,85 @@
+// Added style attribute to HTML's body
 $("body").attr("style", "background-color: white;");
-$("body").append(
-  '<div class="headerContent header py-3 shadow-sm" style="background-color: lightblue">' +
-    "</div>"
+
+// Added Bootstrap classes to HTML's body
+$("body").append('<div class="headerContent header py-3 shadow-sm" style="background-color: lightblue">' + "</div>"
 );
 
+//Crated HTML's header and added Bootstrap classes
 var topEl = $(".headerContent");
 topEl.append('<div class="topRow row mx-auto">' + "</div>");
 
+// Created top row that will contain the two (2) columns that will enclose HTML's title and weather widget
 var topRowEl = $(".topRow");
-topRowEl.append(
-  '<div class="topCol1 col-12 col-md-4 ml-auto text-center" id="dateDiv" style="font-size: 1.5em;">' +
-    "</div>"
-);
-topRowEl.append(
-  '<div class="topCol2 col-12 col-md-1 mr-md-auto my-auto pt-3 pt-md-0 text-center">' +
-    "</div>"
-);
+    topRowEl.append('<div class="topCol1 col-12 col-md-4 ml-auto text-center" id="dateDiv" style="font-size: 1.5em;">' + "</div>");
+    topRowEl.append('<div class="topCol2 col-12 col-md-1 mr-md-auto my-auto pt-3 pt-md-0 text-center">' + "</div>");
 
+// Created header's left column to display pages's title with motto
 var topCol1 = $(".topCol1");
-topCol1.append('<h2 class="display-4">Work Daily Schedule</h2>');
-topCol1.append("<h5>Everyday is a beautiful day!</h5>");
+    topCol1.append('<h2 class="display-4">Work Daily Schedule</h2>');
+    topCol1.append("<h5>Everyday is a beautiful day!</h5>");
 
+// Created header's right column to display page's weather widget (it will go to bottom in mobile view)
 var topCol2 = $(".topCol2");
-topCol2.append(
-  '<a target="_blank" href="https://www.booked.net/weather/new-york-18103"><img src="https://w.bookcdn.com/weather/picture/25_18103_1_1_3498db_250_2980b9_ffffff_ffffff_1_2071c9_ffffff_0_6.png?scode=124&domid=w209&anc_id=9699" style="border-radius: 10px; box-shadow: 0 0 5px gray;" alt="booked.net"/></a>'
-);
+    topCol2.append('<a target="_blank" href="https://www.booked.net/weather/new-york-18103"><img src="https://w.bookcdn.com/weather/picture/25_18103_1_1_3498db_250_2980b9_ffffff_ffffff_1_2071c9_ffffff_0_6.png?scode=124&domid=w209&anc_id=9699" style="border-radius: 10px; box-shadow: 0 0 5px gray;" alt="booked.net"/></a>');
 
+// Created todayDate to enclose Date() along with date's structure (day string, month string, day number and year number)
 var todayDate = new Date(),
-  weekdays = new Array(
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-  );
-months = new Array(
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December"
-);
+    weekdays = new Array( "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+    months = new Array( "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
 
-dayOfWeek = weekdays[todayDate.getDay()];
-curMonth = months[todayDate.getMonth()];
-curYear = todayDate.getFullYear();
-dayOfMonth = todayDate.getDate();
-curHour = todayDate.getHours();
 
-var today =
-  "Today's date: " +
-  dayOfWeek +
-  ", " +
-  curMonth +
-  " " +
-  dayOfMonth +
-  ", " +
-  curYear;
-$("#dateDiv").append(today);
+    dayOfWeek = weekdays[todayDate.getDay()];
+    curMonth = months[todayDate.getMonth()];
+    curYear = todayDate.getFullYear();
+    dayOfMonth = todayDate.getDate();
+    curHour = todayDate.getHours();
 
+// Created today variable to establish a string to be appended to page
+var today = "Today's date: " + dayOfWeek + ", " + curMonth + " " + dayOfMonth + ", " + curYear;
+    
+    // Appended date's string to right (bottom) column.
+    // NOTE: var was not created for this particular element, sine it's not going to be modified later on. 
+    $("#dateDiv").append(today);
+
+// Crated variable to append HTML elements into HTML's body
 var bodyEl = $("body");
 
-// Appending Container
-bodyEl.append('<div class="container">' + "</div>");
+// Appending Container with .container class to body element
+bodyEl.append('<main class="container">' + "</main>");
 
+// Created variable for container Class
 var container = $(".container");
 
-// Apending top row of time
-container.append('<div class="row midRow">' + "</div>");
+// Created and appended section HTML semantic element to .container
+    container.append('<section class="row midRow">' + "</section>");
+
+// Created variable for HTML section element that will have form with AM times
 var midRow = $(".midRow");
-midRow.append('<div class="col-12 mainFrameAM">' + "</div>");
+    midRow.append('<section class="col-12 mainFrameAM">' + "</section>");
 
+// Created var with mainFrameAM class for form to frame label, input and icon ('<i>') tags and append them 
 var mainframeElAM = $(".mainFrameAM");
-mainframeElAM.append('<form class="plannerForm">' + "</form>");
 
+// Created form for AM times
+    mainframeElAM.append('<form class="plannerForm">' + "</form>");
+
+// Created variables for styles that repeat through HTML structure
 var plannerFormElAM = $(".plannerForm");
 var labelStyle = ' col-2 col-md-1 mr-4 mr-md-5 mt-2" ';
 var inputStyle = ' col-8 col-md-9"';
 var inputStylePassed = "'color: black; background-color: #ffb6c1;'";
-var iconStyle =
-  '  style="font-size: 1.25em;" class="far fa-calendar-check col-1" id="i';
+var iconStyle = '  style="font-size: 1.25em;" class="far fa-calendar-check col-1" id="i';
 
-// Appending lables from 6:00AM to 12:00AM
-timeArray = [];
+// Created array that will contain values of times through the planner's structure
+var timeArray = [];
 
+// Created loop for hours from midnight to noon
 for (var tAM = 0; tAM < 13; tAM++) {
-  //Appends labels and input from 1:00AM to 11:00AM
 
+  //Appends labels and input from
   plannerFormElAM.append(
-    '<label class="timeLabel' + labelStyle + 'id="timeLabel' + "[" + tAM + "]" + '">' + [tAM] + ":00" + "</label>"
-  );
+    '<label class="timeLabel' + labelStyle + 'id="timeLabel' + "[" + tAM + "]" + '">' + [tAM] + ":00" + "</label>");
   plannerFormElAM.append(
     '<input class="taskInput' + inputStyle + 'id="' + [tAM] + '"' + ">" + "</input>");
   plannerFormElAM.append("<i" + iconStyle + [tAM] + '">' + "</i>");
@@ -119,7 +100,11 @@ var inputStylePassedPM = "'color: black; background-color: #ffb6c1;'";
 var iconStylePM =
   '  style="font-size: 1.25em;" class="far fa-calendar-check col-1" id="i';
 
+
+
+  
 for (var tPM = 13; tPM < 22; tPM++) {
+
   //Appends labels and input from 1:00AM to 11:00AM
   timePM = [tPM] - 12;
   plannerFormPMEL.append('<label class="timeLabel' + labelStyle + 'id="timeLabel' + "[" + tPM + "]" + '">' + [timePM] + ":00" + "</label>");
@@ -128,7 +113,7 @@ for (var tPM = 13; tPM < 22; tPM++) {
   timeArray.push(tPM);
 }
 
-
+// Establishing for loop through the app to grab i values and iterate them into the changes below
 for (i = 0; i < 24; i++) {
     // Established background color to be changed if time is equal to the current hour
   if (timeArray[i] == curHour) {
